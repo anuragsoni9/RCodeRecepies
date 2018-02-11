c856a19f345e94416fc5ac7f7e05f0387cf729a1
Anurag Soni <soni.anuragg@gmail.com>
2018-02-10 22:15:39 -0500

    Create Plot.R

---
+++ Plot.R
@@ -0,0 +1,3 @@
+#create stepped bar charts without spaces(like Histogram)
+
+barplot(y,space=0,ylim=c(0,0.7),names.arg=c(0,1,2,3,4), main=c("Probability Density"),xlab=c("Buckets"),y=c("Probability"),font.lab=2,col="dodgerblue" )
